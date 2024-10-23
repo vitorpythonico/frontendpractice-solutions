66ef83bb227b182f79e5e318f2599219b4e45b49
Vitor (Pythonic) <vitor.emailcontato@gmail.com>
2024-10-23 16:39:06 -0300

    fix(oh.studio/Header): z-index

--- oh.studio/components/Header/index.js
+++ oh.studio/components/Header/index.js
@@ -7,10 +7,10 @@ export default function Header() {
 	const pathname = usePathname();
 
 	return (
-		<header className="fixed left-[50%] right-[50%] mt-4 text-sm font-sans">
+		<header className="fixed left-[50%] right-[50%] z-10 mt-6 text-sm font-sans">
 			<nav className="flex justify-center">
 				<ul className="flex items-center gap-4 p-1 rounded-full bg-secundary backdrop-blur-md">
-					<li className={`py-2.5 px-6 rounded-full z-10 ${pathname === '/' && 'bg-white'}`}><Link href="/">Home</Link></li>
+					<li className={`py-2.5 px-6 rounded-full ${pathname === '/' && 'bg-white'}`}><Link href="/">Home</Link></li>
 					<li className={`py-2.5 px-6 rounded-full ${pathname === '/profile' && 'bg-white'}`}><Link href="/profile">Profile</Link></li>
 					<li className={`py-2.5 px-6 rounded-full ${pathname === '/contact' && 'bg-white'}`}><Link href="/contact">Contact</Link></li>
 				</ul>
