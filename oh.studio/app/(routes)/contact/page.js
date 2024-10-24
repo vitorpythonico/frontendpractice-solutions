d041bf3a1aa367ec0e66f7fa129efdf4f10e8dcc
Vitor (Pythonic) <vitor.emailcontato@gmail.com>
2024-10-24 16:09:57 -0300

    feat(oh.studio/contact): add responsive version

--- oh.studio/app/(routes)/contact/page.js
+++ oh.studio/app/(routes)/contact/page.js
@@ -7,8 +7,8 @@ export const metadata = {
 
 export default function Contact() {
 	return (
-		<main className="h-[80vh] flex items-center font-sans mx-5 pt-28">
-			<h1 className="w-full text-center text-4xl leading-tight font-medium">
+		<main className="h-[80vh] flex items-center font-sans mx-5 pt-28 md:h-[90vh]">
+			<h1 className="w-full text-center text-4xl leading-tight font-medium md:text-5xl lg:text-7xl lg:leading-tight">
 				<span className="block">Let's work together.</span>
 				<span className="text-neutral"><Link href="mailto:oli@oh.studio">Get in touch.</Link></span>
 			</h1>
